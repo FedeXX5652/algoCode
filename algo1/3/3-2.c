1bdd30ed182f938a60487db182ddf59e49217f2b
FedeXX5652 <fedegalant@gmail.com>
2022-04-05 11:03:39 -0300

    intercambiar valores

--- algo1/3/3-2.c
+++ algo1/3/3-2.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-int function(int nro, int nro2)
-{
-    printf("La suma de %d y %d es: %d\n", nro, nro2, (nro+nro2));
-    return 0;
+// las funciones tienen return, los procedimientos no (llevan void)
+
+char* function(int* ref_n1, int* ref_n2) {      // le pasamos punteros
+    printf("punteros a int: %i %i\n", *ref_n1, *ref_n2);    // accedemos a los valores de los punteros
+    int n3 = (*ref_n1);
+    (*ref_n1) = (*ref_n2);
+    (*ref_n2) = n3;
+    return "Ok";
 }
 
 int main()
 {
-
     int nro;
     int nro2;
+    char* function_response;
+
     printf("Ingrese un numero: ");
-    scanf("%i", &nro);
+    scanf("%i", &nro);      // el & permite que el valor ingresado se guarde en la variable nro
     printf("Ingrese otro numero: ");
     scanf("%i", &nro2);
     
-    function(nro, nro2);
+    function_response = function(&nro, &nro2);
+    printf("-------------------------------------------\nLa funcion se ejecutó con: %s\n", function_response);
+    printf("Nros finales intercambiados: %i, %i\n", nro, nro2);
     return 0;
-
 }

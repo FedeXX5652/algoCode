c7054e6882f2ea8e7894553a03522e89b1578489
FedeXX5652 <fedegalant@gmail.com>
2022-04-30 20:41:32 -0300

    AVANCE SERIO TP2

--- algo1/tp2/papeleo.c
+++ algo1/tp2/papeleo.c
@@ -14,9 +14,14 @@ void imprimir_terreno(juego_t juego){
     printf("MOVIMIENTOS: %d\n", juego.jugador.movimientos);
     printf("TOPE PAREDES: %d\n", juego.niveles[juego.nivel_actual-1].tope_paredes);
 
+    for(int i = 0; i < DIM_NIVEL_1; i++){
+        for(int j = 0; j < DIM_NIVEL_1; j++){
+            terreno[i][j] = ' ';
+        }
+    }
+
     for(int i=0; i<juego.niveles[juego.nivel_actual-1].tope_paredes; i++){
-        printf("%d, %d ES PARED\n", juego.niveles[0].paredes[i].fil, juego.niveles[0].paredes[i].col);
-        terreno[juego.niveles[0].paredes[i].fil][juego.niveles[0].paredes[i].col] = '|';
+        terreno[juego.niveles[0].paredes[i].fil][juego.niveles[0].paredes[i].col] = '#';
     }
 
     for(int i=0; i<DIM_NIVEL_1; i++){

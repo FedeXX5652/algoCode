18a80d9a27e2b6ec62b6efaeb0c487fd3b48ff8b
FedeXX5652 <fedegalant@gmail.com>
2022-08-23 14:29:37 -0300

    cpp start

---
+++ algo2/1/1.cpp
@@ -0,0 +1,16 @@
+// -C esta contenido en C++ (C++ es un superset de C)
+// -estandares: ANSI C++ / ISO C++
+
+// Compilador:
+//     -Eclipse
+//     -Code::Blocks
+//     -Dev C++
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;     // imprime lo que hay dsps del cout <<
+    return 0;
+}
